69ebbcb9245f35ecb4beb20c5faa34982bd956fc
Oleg Koskin <oleg.koskin@gmail.com>
2017-02-03 01:50:56 +0200

    Implement RandomizedQueue

--- queues/src/main/java/com/github/olegkoskin/algs4/queue/RandomizedQueue.java
+++ queues/src/main/java/com/github/olegkoskin/algs4/queue/RandomizedQueue.java
@@ -1,54 +1,139 @@
 package com.github.olegkoskin.algs4.queue;
 
+import java.util.Arrays;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class RandomizedQueue<E> implements Iterable<E> {
 
+    private Object[] array;
+    private int size;
+
     public RandomizedQueue() {
+        array = new Object[1];
     }
 
     /**
      * @return true is the queue is empty
      */
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     /**
      * @return the number of items on the queue
      */
     public int size() {
-        return 0;
+        return size;
     }
 
     /**
      * add the item
      *
-     * @param e
+     * @param e to add
      */
     public void enqueue(E e) {
+        Objects.requireNonNull(e);
+
+        if (size == array.length) {
+            resize(size * 2);
+        }
+
+        array[size++] = e;
     }
 
     /**
-     * remove and return a random item
      *
-     * @return
+     * @return random item and remove it
      */
+    @SuppressWarnings("unchecked")
     public E dequeue() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        int index = ThreadLocalRandom.current().nextInt(size);
+        Object old = array[index];
+
+        array[index] = null;
+        size--;
+
+        if (size > 0 && size == array.length / 4) {
+            resize(array.length / 2);
+        }
+
+        return (E) old;
     }
 
     /**
      * @return (but do not remove) a random item
      */
+    @SuppressWarnings("unchecked")
     public E sample() {
-        return null;
+        if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+
+        return (E) array[ThreadLocalRandom.current().nextInt(size)];
     }
 
     /**
      * @return an independent iterator over items in random order
      */
     public Iterator<E> iterator() {
-        return null;
+        return new RandomIterator();
+    }
+
+    private void resize(int capacity) {
+        array = Arrays.copyOf(array, capacity);
+    }
+
+    private class RandomIterator implements Iterator<E> {
+        private int cursor;
+        private int[] randomIndexes = new int[size];
+
+        RandomIterator() {
+            ThreadLocalRandom localRandom = ThreadLocalRandom.current();
+            for (int i = 0; i < size; i++) {
+                int randomIndex = -1;
+
+                while (exists(randomIndex, randomIndexes)) {
+                    randomIndex = localRandom.nextInt(size);
+                }
+
+                randomIndexes[i] = randomIndex;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return cursor < size;
+        }
+
+        @SuppressWarnings("unchecked")
+        @Override
+        public E next() {
+            if (cursor >= size) {
+                throw new NoSuchElementException();
+            }
+
+            return (E) array[randomIndexes[cursor++]];
+        }
+
+        private boolean exists(int number, int[] array) {
+            if (number == -1) {
+                return true;
+            }
+
+            for (int anArray : array) {
+                if (number == anArray) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
     }
 }
